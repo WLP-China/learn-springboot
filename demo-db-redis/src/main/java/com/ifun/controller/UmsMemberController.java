80f2647e2a1bbc9ac3464d69c6143a8726cbf92e
wanglongpeng <ifunning@163.com>
2019-12-13 17:26:25 +0800

    demo-redis

---
+++ demo-db-redis/src/main/java/com/ifun/controller/UmsMemberController.java
@@ -0,0 +1,46 @@
+package com.ifun.controller;
+
+import com.ifun.common.api.CommonResult;
+import com.ifun.service.UmsMemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * 会员登录注册管理Controller
+ */
+@Controller
+@RequestMapping("/sso")
+public class UmsMemberController {
+    @Autowired
+    private UmsMemberService memberService;
+
+    /**
+     * 获取验证码
+     *
+     * @param telephone
+     * @return
+     */
+    @RequestMapping(value = "/getAuthCode", method = RequestMethod.GET)
+    @ResponseBody
+    public CommonResult getAuthCode(@RequestParam String telephone) {
+        return memberService.generateAuthCode(telephone);
+    }
+
+    /**
+     * 判断验证码是否正确
+     *
+     * @param telephone
+     * @param authCode
+     * @return
+     */
+    @RequestMapping(value = "/verifyAuthCode", method = RequestMethod.POST)
+    @ResponseBody
+    public CommonResult updatePassword(@RequestParam String telephone,
+                                       @RequestParam String authCode) {
+        return memberService.verifyAuthCode(telephone, authCode);
+    }
+}
