4b05c13a89e4e842eeccdf59f4e119dff2132bce
MQing <ifunning@163.com>
2020-06-04 23:08:43 +0800

    +

--- demo-security-jwt/src/main/java/com/ifun/component/JwtAuthenticationTokenFilter.java
+++ demo-security-jwt/src/main/java/com/ifun/component/JwtAuthenticationTokenFilter.java
@@ -29,6 +29,7 @@ public class JwtAuthenticationTokenFilter extends OncePerRequestFilter {
     private UserDetailsService userDetailsService;
     @Autowired
     private JwtTokenUtil jwtTokenUtil;
+
     @Value("${jwt.tokenHeader}")
     private String tokenHeader;
     @Value("${jwt.tokenHead}")
