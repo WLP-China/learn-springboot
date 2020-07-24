b7c37af00d1813eb7015801c7b417c03b65b1ded
MuQing <ifunning@163.com>
2020-07-24 10:19:15 +0800

    拼写错误、漏删

--- pro-website/src/main/java/com/muqing/dao/UserDao.java
+++ pro-website/src/main/java/com/muqing/dao/UserDao.java
@@ -16,7 +16,7 @@ import org.apache.ibatis.annotations.Update;
 public interface UserDao {
 
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    @Insert("insert into sys_user(username, password, name, phone, eid, eName, status, createTime, updateTime) values(#{username}, #{password}, #{name}, #{phone}, #{eid}, #{eName}, #{status}, now(), now())")
+    @Insert("insert into sys_user(username, password, name, phone, status, createTime, updateTime) values(#{username}, #{password}, #{name}, #{phone}, #{status}, now(), now())")
     int save(SysUser user);
 
     @Select("select * from sys_user t where t.id = #{id}")
